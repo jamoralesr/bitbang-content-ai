e87a4023e7c3ddd16b5778999f1e69ebc5c3801f
Jaime Morales <jamoralesr@gmail.com>
2025-02-10 12:53:20 -0300

    Modifica prompt para solucionar resumenes encerrados entre comillas

--- app/Console/Commands/ProcessPostsContentsWithIa.php
+++ app/Console/Commands/ProcessPostsContentsWithIa.php
@@ -33,18 +33,23 @@ class ProcessPostsContentsWithIa extends Command
         $count = 0;
 
         foreach ($posts as $post) {
-            $prompt = "Eres un escritor y periodista experto en sintetizar textos y crear extractos a partir de otros contenidos. Necesito un resumen con un enfoque periodístico del texto indicado mas adelante en 55 palabras, con el objetivo de hacerlo mas corto en máximo un parrafo, usando el mismo tono que se usó originalmente en el texto, no repitas frases y usa un lenguaje natural y variado, asegurándote de que el mensaje sea claro y profesional. Sin añadir introducciones, comentarios o títulos de ninguno tipo. Asegúrate de que el resultado final tenga entre 40 y 55 palabras como mínimo y máximo. Si es necesario, prioriza la concisión sin omitir ideas clave. Estructura el contenido como un resumen breve y atractivo, con un lead potente ya que las primeras líneas deben captar la atención. Es muy importante que el texto final no sobrepase las 55 palabras y que no se componga de mas de un parrafo, ya que ese es el objetivo. Para generar el contenido de resumen, debes usar la siguiente información: Título: {$post->titulo} y Contenido: {$post->texto_descriptivo_sin_html}";
+            // Solo procesar si el resumen está vacío
+            if (empty($post->resumen)) {
+                $prompt = "Eres un escritor y periodista experto en sintetizar textos y crear extractos a partir de otros contenidos. Necesito un resumen con un enfoque periodístico del texto indicado mas adelante en 55 palabras, con el objetivo de hacerlo mas corto en máximo un parrafo, usando el mismo tono que se usó originalmente en el texto, no repitas frases y usa un lenguaje natural y variado, asegurándote de que el mensaje sea claro y profesional. Sin añadir introducciones, comentarios o títulos de ninguno tipo. Asegúrate de que el resultado final tenga entre 40 y 55 palabras como mínimo y máximo. Si es necesario, prioriza la concisión sin omitir ideas clave. Estructura el contenido como un resumen breve y atractivo, con un lead potente ya que las primeras líneas deben captar la atención. Es muy importante que el texto final sea un texto directo, sin comillas, limitado a 55 palabras, en un solo párrafo. Para generar el contenido de resumen, debes usar la siguiente información: Título: {$post->titulo} y Contenido: {$post->texto_descriptivo_sin_html}";
 
-            $resumen = $this->processPrompts($prompt);
+                $resumen = $this->processPrompts($prompt);
 
-            $post->update(
-                [
-                    'resumen' => $resumen,
-                ],
-            );
+                $post->update(
+                    [
+                        'resumen' => $resumen,
+                    ],
+                );
 
-            $count++;
-            $this->info("Se ha creado el resumen para el post {$post->id} de {$posts->count()}");
+                $count++;
+                $this->info("Se ha creado el resumen para el post {$post->id} de {$posts->count()}");
+            } else {
+                $this->info("El post {$post->id} ya tiene resumen, se omite.");
+            }
         }
 
         $this->info("Proceso finalizado. {$count} posts actualizados con sus resumenes");
